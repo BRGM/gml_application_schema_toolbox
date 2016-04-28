62e4826261c3733094d5af1a739d4ad87eff8872
Hugo Mercier <hugo.mercier@oslandia.com>
2016-04-28 10:38:08 +0200

    Use gettempdir()

--- qgis_complex_features/gml2relational/test_model_builder.py
+++ qgis_complex_features/gml2relational/test_model_builder.py
@@ -4,6 +4,7 @@ import unittest
 
 import os
 import sys
+import tempfile
 sys.path = [os.path.join(os.path.dirname(__file__), "../whl/all")] + sys.path
 
 from relational_model_builder import load_gml_model
@@ -22,7 +23,7 @@ class TestModelBuilder(unittest.TestCase):
         self.assertTrue(new_fields == set(), "New fields : {} got {} expected {}".format(new_fields, trepr, torig_repr))
     
     def test1a(self):
-        model = load_gml_model(os.path.join(testdata_path, "t1.xml"), "/tmp")
+        model = load_gml_model(os.path.join(testdata_path, "t1.xml"), tempfile.gettempdir())
         self.assertEqual(len(model.tables()), 2)
         ta = set(["Column<d/text(),TEXT>",
                   "Link<c/cdetails/name(1-*),a_c_cdetails_name>",
@@ -39,7 +40,7 @@ class TestModelBuilder(unittest.TestCase):
 
     def test1b(self):
         # with sequence merging
-        model = load_gml_model(os.path.join(testdata_path, "t1.xml"), "/tmp", merge_sequences = True)
+        model = load_gml_model(os.path.join(testdata_path, "t1.xml"), tempfile.gettempdir(), merge_sequences = True)
         self.assertEqual(len(model.tables()), 1)
         t = set(["Column<d/text(),TEXT>",
                  "Column<c/cdetails/name[0]/prefix/text(),TEXT,optional>",
@@ -51,7 +52,7 @@ class TestModelBuilder(unittest.TestCase):
 
     def test1c(self):
         # with 0 max merge depth
-        model = load_gml_model(os.path.join(testdata_path, "t1.xml"), "/tmp", merge_max_depth = 0)
+        model = load_gml_model(os.path.join(testdata_path, "t1.xml"), tempfile.gettempdir(), merge_max_depth = 0)
         t1 = set(["Link<c(1-1),a_c>","Link<b(1-1),a_b>","Link<e(1-1),a_e>","Column<@id,None,autoincremented>","Link<d(1-1),a_d>"])
         self.assertTableEqualRepr(model.tables()['a'], t1)
         t2 = set(["Link<prefix(0-1),a_c_cdetails_name_prefix>","Column<@id,None,autoincremented>","BackLink<name(a_c_cdetails)>"])
@@ -72,7 +73,7 @@ class TestModelBuilder(unittest.TestCase):
         self.assertTableEqualRepr(model.tables()['a_e'], t9)
 
     def test2(self):
-        model = load_gml_model(os.path.join(testdata_path, "t2.xml"), "/tmp", merge_max_depth = 6, merge_sequences = False)
+        model = load_gml_model(os.path.join(testdata_path, "t2.xml"), tempfile.gettempdir(), merge_max_depth = 6, merge_sequences = False)
         self.assertEqual(len(model.tables()), 2)
         ta = set(["Column<d/text(),TEXT>",
                   "Link<c/cdetails/name(1-*),a_c_cdetails_name>",
@@ -88,13 +89,13 @@ class TestModelBuilder(unittest.TestCase):
         self.assertTableEqualRepr(model.tables()['a_c_cdetails_name'], tb)        
 
         # it should be the same with sequence merging
-        model = load_gml_model(os.path.join(testdata_path, "t2.xml"), "/tmp", merge_max_depth = 6, merge_sequences = True)
+        model = load_gml_model(os.path.join(testdata_path, "t2.xml"), tempfile.gettempdir(), merge_max_depth = 6, merge_sequences = True)
         self.assertEqual(len(model.tables()), 2)
         self.assertTableEqualRepr(model.tables()['a'], ta)
         self.assertTableEqualRepr(model.tables()['a_c_cdetails_name'], tb)        
 
     def test3(self):
-        model = load_gml_model(os.path.join(testdata_path, "t3.xml"), "/tmp", merge_sequences = True)
+        model = load_gml_model(os.path.join(testdata_path, "t3.xml"), tempfile.gettempdir(), merge_sequences = True)
         t = set(["Column<d/text(),TEXT>",
                  "Column<c/cdetails/name[0]/prefix/text(),TEXT,optional>",
                  "Column<c/type/text(),TEXT>",
@@ -107,7 +108,7 @@ class TestModelBuilder(unittest.TestCase):
         
     def test4(self):
         # tables with ID => unmergeable
-        model = load_gml_model(os.path.join(testdata_path, "t4.xml"), "/tmp", merge_sequences = True)
+        model = load_gml_model(os.path.join(testdata_path, "t4.xml"), tempfile.gettempdir(), merge_sequences = True)
         ta = set(["Link<g(0-1),a_g_t>",
                   "Column<d/text(),TEXT>",
                   "Link<h(0-1),MyHType>",
@@ -123,7 +124,7 @@ class TestModelBuilder(unittest.TestCase):
 
     def xtest_geom1(self):
         # schema with a geometry
-        model = load_gml_model(os.path.join(testdata_path, "t5.xml"), "/tmp", merge_sequences = True)
+        model = load_gml_model(os.path.join(testdata_path, "t5.xml"), tempfile.gettempdir(), merge_sequences = True)
         for table in model.tables().values():
             print(table)
         t = set(["Column<name/text(),TEXT>","Geometry<location/Point,Point(4326)>","Column<@id,None,autoincremented>"])
