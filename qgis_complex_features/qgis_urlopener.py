6d58ba6572f9048fe95acaf9190d03512fb09b09
Hugo Mercier <hugo.mercier@oslandia.com>
2016-06-14 15:45:05 +0200

    Fix the url downloader

--- qgis_complex_features/qgis_urlopener.py
+++ qgis_complex_features/qgis_urlopener.py
@@ -1,16 +1,21 @@
 from PyQt4.QtCore import QUrl, QEventLoop
-from PyQt4.QtNetwork import QNetworkRequest
+from PyQt4.QtNetwork import QNetworkRequest, QNetworkAccessManager
 from StringIO import StringIO
 from qgis.core import QgsNetworkAccessManager
 
+__network_manager = None
+
 def remote_open_from_qgis(uri):
     """Opens a remote URL using QGIS proxy preferences"""
-    nm = QgsNetworkAccessManager.instance()
+    global __network_manager
+    if __network_manager is None:
+        __network_manager = QNetworkAccessManager()
+        __network_manager.setProxy(QgsNetworkAccessManager.instance().proxy())
     pause = QEventLoop()
     req = QNetworkRequest(QUrl.fromEncoded(uri))
     req.setRawHeader("Accept", "application/xml")
     req.setRawHeader("Accept-Language", "fr")
-    reply = nm.get(req)
+    reply = __network_manager.get(req)
     reply.finished.connect(pause.quit)
     def onError(self):
         pause.quit()
