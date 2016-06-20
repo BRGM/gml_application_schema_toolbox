11f821f47a13abcd664d1a67be520793ec586efc
Hugo Mercier <hugo.mercier@oslandia.com>
2016-06-20 15:40:18 +0200

    Version 0.8.0

--- gml_application_schema_toolbox/__init__.py
+++ gml_application_schema_toolbox/__init__.py
@@ -5,7 +5,7 @@ def name():
 def description():
     return u"QGIS GML Application Schema Toolbox"
 def version():
-    return u"0.7.3"
+    return u"0.8.0"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
