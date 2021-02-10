71661e4dddb5715ad521310cc4c187aff5570efb
Julien M <julien.moura@oslandia.com>
2021-02-10 22:15:25 +0100

    Remove python 2 artifacts

--- gml_application_schema_toolbox/gui/xml_tree_widget.py
+++ gml_application_schema_toolbox/gui/xml_tree_widget.py
@@ -13,7 +13,6 @@
 #   You should have received a copy of the GNU Library General Public
 #   License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import
 
 import xml.etree.ElementTree as ET
 from builtins import next, range
--- gml_application_schema_toolbox/viewers/wml2_timeseries.py
+++ gml_application_schema_toolbox/viewers/wml2_timeseries.py
@@ -13,9 +13,7 @@
 #   You should have received a copy of the GNU Library General Public
 #   License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import
 
-import os
 import time
 from builtins import object, str
 from datetime import datetime
