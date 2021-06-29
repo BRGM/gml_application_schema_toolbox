02c0a18dbe0a971fa5f2662c511c6648f48fc3f0
Julien M <julien.moura@oslandia.com>
2021-06-29 17:36:53 +0200

    Replace old settings loader

--- gml_application_schema_toolbox/core/qgis_urlopener.py
+++ gml_application_schema_toolbox/core/qgis_urlopener.py
@@ -11,8 +11,7 @@ from typing import Dict
 
 # project
 from gml_application_schema_toolbox.__about__ import __title__
-from gml_application_schema_toolbox.core.settings import settings
-from gml_application_schema_toolbox.toolbelt.log_handler import PlgLogger
+from gml_application_schema_toolbox.toolbelt import PlgLogger, PlgOptionsManager
 from gml_application_schema_toolbox.toolbelt.network_manager import (
     NetworkAccessManager,
     RequestsException,
@@ -25,7 +24,7 @@ from gml_application_schema_toolbox.toolbelt.network_manager import (
 __network_manager = None
 logger = logging.getLogger(__name__)
 plg_logger = PlgLogger()
-
+plg_settings = PlgOptionsManager().get_plg_settings()
 
 # ############################################################################
 # ########## Functions #############
@@ -36,8 +35,8 @@ def remote_open_from_qgis(
     uri: str,
     headers: Dict[bytes, bytes] = {
         b"Accept": b"application/xml",
-        b"Accept-Language": bytes(settings.value("default_language", "fr"), "utf8"),
-        b"User-Agent": bytes(settings.value("http_user_agent", __title__), "utf8"),
+        b"Accept-Language": bytes(plg_settings.network_language, "utf8"),
+        b"User-Agent": bytes(plg_settings.network_http_user_agent, "utf8"),
     },
 ) -> BytesIO:
     """Opens a remote URL using Network Acess Manager. In fact, just a shortcut.
