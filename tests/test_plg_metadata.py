c3970e6f9d1b93945ab2eb3c080121942de1bd2a
Julien M <julien.moura@oslandia.com>
2021-07-01 20:26:48 +0200

    Fix about and enlarge md test

--- tests/test_plg_metadata.py
+++ tests/test_plg_metadata.py
@@ -12,6 +12,7 @@
 
 # standard library
 import unittest
+from pathlib import Path
 
 # 3rd party
 from semver import VersionInfo
@@ -25,6 +26,48 @@ from gml_application_schema_toolbox import __about__
 
 
 class TestPluginMetadata(unittest.TestCase):
+    def test_metadata_types(self):
+        """Test types."""
+        # plugin metadata.txt file
+        self.assertIsInstance(__about__.PLG_METADATA_FILE, Path)
+        self.assertTrue(__about__.PLG_METADATA_FILE.is_file())
+
+        # plugin dir
+        self.assertIsInstance(__about__.DIR_PLUGIN_ROOT, Path)
+        self.assertTrue(__about__.DIR_PLUGIN_ROOT.is_dir())
+
+        # metadata as dict
+        self.assertIsInstance(__about__.__plugin_md__, dict)
+
+        # general
+        self.assertIsInstance(__about__.__author__, str)
+        self.assertIsInstance(__about__.__copyright__, str)
+        self.assertIsInstance(__about__.__email__, str)
+        self.assertIsInstance(__about__.__keywords__, list)
+        self.assertIsInstance(__about__.__license__, str)
+        self.assertIsInstance(__about__.__summary__, str)
+        self.assertIsInstance(__about__.__title__, str)
+        self.assertIsInstance(__about__.__title_clean__, str)
+        self.assertIsInstance(__about__.__version__, str)
+        self.assertIsInstance(__about__.__version_info__, tuple)
+
+        # misc
+        self.assertLessEqual(len(__about__.__title_clean__), len(__about__.__title__))
+
+        # QGIS versions
+        self.assertIsInstance(
+            __about__.__plugin_md__.get("general").get("qgisminimumversion"), str
+        )
+
+        self.assertIsInstance(
+            __about__.__plugin_md__.get("general").get("qgismaximumversion"), str
+        )
+
+        self.assertLessEqual(
+            float(__about__.__plugin_md__.get("general").get("qgisminimumversion")),
+            float(__about__.__plugin_md__.get("general").get("qgismaximumversion")),
+        )
+
     def test_version_semver(self):
         """Test if version comply with semantic versioning."""
         self.assertTrue(VersionInfo.isvalid(__about__.__version__))
