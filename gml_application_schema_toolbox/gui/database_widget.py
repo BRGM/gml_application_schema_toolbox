c48067cd8f7a0506dfbf7afcbdb1715fd2863f2e
Julien M <julien.moura@oslandia.com>
2021-07-02 20:28:15 +0200

    Simpligy db form

--- gml_application_schema_toolbox/gui/database_widget.py
+++ gml_application_schema_toolbox/gui/database_widget.py
@@ -27,56 +27,64 @@ WIDGET, BASE = uic.loadUiType(DIR_PLUGIN_ROOT / "ui/{}.ui".format(Path(__file__)
 # ##################################
 
 
-class PgsqlConnectionsModel(QAbstractItemModel):
-    def __init__(self, parent=None):
-        super(PgsqlConnectionsModel, self).__init__(parent)
+# class PgsqlConnectionsModel(QAbstractItemModel):
+#     def __init__(self, parent=None):
+#         super(PgsqlConnectionsModel, self).__init__(parent)
 
-        self._settings = QSettings()
-        self._settings.beginGroup("/PostgreSQL/connections/")
+#         self._settings = QSettings()
+#         self._settings.beginGroup("/PostgreSQL/connections/")
 
-    def _groups(self):
-        return self._settings.childGroups()
+#     def _groups(self):
+#         return self._settings.childGroups()
 
-    def parent(self, index):
-        return QModelIndex()
+#     def parent(self, index):
+#         return QModelIndex()
 
-    def index(self, row, column, parent):
-        return self.createIndex(row, column)
+#     def index(self, row, column, parent):
+#         return self.createIndex(row, column)
 
-    def rowCount(self, parent):
-        return len(self._groups())
+#     def rowCount(self, parent):
+#         return len(self._groups())
 
-    def columnCount(self, parent):
-        return 1
+#     def columnCount(self, parent):
+#         return 1
 
-    def data(self, index, role=Qt.DisplayRole):
-        return self._groups()[index.row()]
+#     def data(self, index, role=Qt.DisplayRole):
+#         return self._groups()[index.row()]
 
 
 class DatabaseWidget(BASE, WIDGET):
     def __init__(self, parent=None, is_input=False):
         super(DatabaseWidget, self).__init__(parent)
-        self.setupUi(self)
         self.log = PlgLogger().log
-        plg_settings = PlgOptionsManager().get_plg_settings()
-        self.set_accept_mode(QFileDialog.AcceptOpen)
+        self.setupUi(self)
 
         self._pgsql_db = None
-        self._is_input = is_input
-        if not is_input:
-            self.sqlitePathLineEdit.setPlaceholderText("Create a temporary file")
 
-        self.pgsqlFormWidget.setVisible(False)
-        self.pgsqlConnectionsBox.setModel(PgsqlConnectionsModel())
-        self.pgsqlConnectionsRefreshButton.setIcon(
-            QgsApplication.getThemeIcon("/mActionRefresh.svg")
+        # icons
+        self.btn_refresh_connections.setIcon(
+            QgsApplication.getThemeIcon("mActionRefresh.svg")
         )
-        self.addFkeysButton.setIcon(QgsApplication.getThemeIcon("/mActionAdd.svg"))
-        self.dropFkeysButton.setIcon(QgsApplication.getThemeIcon("/mActionRemove.svg"))
+        self.btn_foreign_keys_add.setIcon(
+            QgsApplication.getThemeIcon("/mActionAdd.svg")
+        )
+        self.btn_foreign_keys_del.setIcon(
+            QgsApplication.getThemeIcon("/mActionRemove.svg")
+        )
 
-        self.set_format(plg_settings.db_type_as_str)
-        self.populate_connections_combobox()
+        # connect widgets to methods
         self.cbb_connections.activated.connect(self.switch_form_according_database_type)
+        self.btn_refresh_connections.pressed.connect(self.populate_connections_combobox)
+        # self.btn_foreign_keys_add.clicked.connect(
+        #     self._pgsql_db.add_foreign_key_constraints
+        # )
+        # self.btn_foreign_keys_del.clicked.connect(
+        #     self._pgsql_db.drop_foreign_key_constraints
+        # )
+
+        # fill widgets
+        self.populate_connections_combobox()
+        self.switch_form_according_database_type()
 
     def populate_connections_combobox(self):
         """List existing database connections into the combobox."""
@@ -124,118 +132,18 @@ class DatabaseWidget(BASE, WIDGET):
         selected_conn = self.cbb_connections.itemData(
             self.cbb_connections.currentIndex()
         )
-        if selected_conn.providerKey().startswith("postgr"):
-            self.pgsqlFormWidget.setVisible(True)
-            self.sqliteFormWidget.setVisible(False)
-
-            # list schemas
-            self.pgsqlSchemaBox.clear()
-            for schema_name in sorted(selected_conn.schemas()):
-                self.pgsqlSchemaBox.addItem(schema_name)
-        elif selected_conn.providerKey().startswith("spatialite"):
-            self.pgsqlFormWidget.setVisible(False)
-            self.sqliteFormWidget.setVisible(True)
-
-    def set_accept_mode(self, accept_mode):
-        """QFileDialog.AcceptOpen or QFileDialog.AcceptSave"""
-        self._accept_mode = accept_mode
-        self.pgsqlSchemaBox.setEditable(accept_mode == QFileDialog.AcceptSave)
-
-    @pyqtSlot(bool)
-    def on_sqliteRadioButton_toggled(self, checked):
-        self.sqliteFormWidget.setVisible(self.sqliteRadioButton.isChecked())
-
-    @pyqtSlot(bool)
-    def on_pgsqlRadioButton_toggled(self, checked):
-        self.pgsqlFormWidget.setVisible(self.pgsqlRadioButton.isChecked())
-
-    @pyqtSlot()
-    def on_sqlitePathButton_clicked(self):
-        current_path = self.sqlitePathLineEdit.text()
-
-        if self._accept_mode == QFileDialog.AcceptOpen:
-            filepath, suffix_filter = QFileDialog.getOpenFileName(
-                parent=self,
-                caption=self.tr("Open SQLite database"),
-                directory=current_path,
-                filter=self.tr("SQLite Files (*.sqlite)"),
-            )
-        else:
-            filepath, suffix_filter = QFileDialog.getSaveFileName(
-                parent=self,
-                caption=self.tr("Save to SQLite database"),
-                directory=current_path,
-                filter=self.tr("SQLite Files (*.sqlite)"),
-            )
-            if filepath:
-                filepath = Path(filepath)
-                if filepath.suffix != ".sqlite":
-                    filepath = Path(str(filepath) + ".sqlite")
-                self.sqlitePathLineEdit.setText(str(filepath.resolve()))
-
-    @pyqtSlot(str)
-    def on_pgsqlConnectionsBox_currentIndexChanged(self, text):
-        if self.pgsqlConnectionsBox.currentIndex() == -1:
-            self._pgsql_db = None
-        else:
-            self._pgsql_db = GmlasPostgisDB.from_name(
-                self.pgsqlConnectionsBox.currentText()
-            )
-
-        self.pgsqlSchemaBox.clear()
-        if self._pgsql_db is None:
+        # ignore if it's the placeholder
+        if isinstance(selected_conn, str):
+            self.pgsqlFormWidget.setEnabled(False)
             return
-        schemas = sorted([schema[1] for schema in self._pgsql_db.list_schemas()])
-        for schema in schemas:
-            self.pgsqlSchemaBox.addItem(schema)
 
-    @pyqtSlot()
-    def on_pgsqlConnectionsRefreshButton_clicked(self):
-        self.pgsqlConnectionsBox.setModel(PgsqlConnectionsModel())
-
-    @pyqtSlot()
-    def on_addFkeysButton_clicked(self):
-        self._pgsql_db.add_foreign_key_constraints(self.schema())
-
-    @pyqtSlot()
-    def on_dropFkeysButton_clicked(self):
-        self._pgsql_db.drop_foreign_key_constraints(self.schema())
-
-    def set_format(self, value):
-        if value == "sqlite":
-            self.sqliteRadioButton.setChecked(True)
-        if value == "postgresql":
-            self.pgsqlRadioButton.setChecked(True)
-
-    def get_db_format(self):
-        if self.sqliteRadioButton.isChecked():
-            return "sqlite"
-        if self.pgsqlRadioButton.isChecked():
-            return "postgresql"
-
-    def datasource_name(self):
-        if self.sqliteRadioButton.isChecked():
-            path = self.sqlitePathLineEdit.text()
-            if path == "" and self._is_input:
-                raise InputError("You must select a SQLite file")
-
-            return path
-        if self.pgsqlRadioButton.isChecked():
-            if self._pgsql_db is None:
-                raise InputError("You must select a PostgreSQL connection")
-            return "PG:{}".format(self._pgsql_db.uri.connectionInfo(True))
-
-    def schema(self, create=False):
-        schema = self.pgsqlSchemaBox.currentText()
-        if not create:
-            return schema
-        # if self.pgsqlSchemaBox.currentIndex() == -1:
-        schemas = [schema[1] for schema in self._pgsql_db.list_schemas()]
-        if schema not in schemas:
-            res = QMessageBox.question(
-                self, __title__, self.tr('Create schema "{}" ?').format(schema)
-            )
-            if res != QMessageBox.Yes:
-                raise InputError()
-            self._pgsql_db.create_schema(schema)
-        return schema
+        if selected_conn.providerKey().startswith("postgr"):
+            self.pgsqlFormWidget.setEnabled(True)
+            # list schemas
+            self.cbb_schemas.clear()
+            for schema_name in sorted(selected_conn.schemas()):
+                self.cbb_schemas.addItem(schema_name)
+        elif selected_conn.providerKey().startswith("spatialite"):
+            self.pgsqlFormWidget.setEnabled(False)
+        else:
+            pass
