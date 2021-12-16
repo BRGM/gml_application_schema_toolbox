1ed8a46a3695164f558e94636a636ffe28eeb22d
Benoît Blanc <benoit.blanc@oslandia.com>
2021-12-16 16:26:39 +0100

    Fix about

--- gml_application_schema_toolbox/__about__.py
+++ gml_application_schema_toolbox/__about__.py
@@ -55,6 +55,7 @@ __author__ = __plugin_md__.get("general").get("author")
 __copyright__ = "2014 - {0}, {1}".format(date.today().year, __author__)
 __email__ = __plugin_md__.get("general").get("email")
 __icon_path__ = DIR_PLUGIN_ROOT.resolve() / __plugin_md__.get("general").get("icon")
+__keywords__ = __plugin_md__.get("general").get("repository").split("tags")
 __license__ = "GPL-2.0"
 __summary__ = "{}\n{}".format(
     __plugin_md__.get("general").get("description"),
